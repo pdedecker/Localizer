fb27e0a9a02748548a2eea89743c6d2fc05427cb
peter <peter.dedecker@hotmail.com>
2011-02-28 17:40:47 -0500

    Add a new MatrixRecycler class that will act as a memory pool for areas where lots of identical matrices are being allocated

---
+++ PALM_analysis_MatrixRecycler.h
@@ -0,0 +1,26 @@
+/*
+ *  PALM_analysis_MatrixRecycler.h
+ *  PALM analysis XOP
+ *
+ *  Created by Peter Dedecker on 28/02/2011.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include <list>
+#include "boost/thread.hpp"
+#include <Eigen/Eigen>
+
+class MatrixRecycler {
+	MatrixRecycler() {;}
+	~MatrixRecycler();
+	
+	Eigen::MatrixXd *getMatrix(size_t nRows, size_t nCols);
+	void freeMatrix(Eigen::MatrixXd *matrixToFree);
+	
+protected:
+	std::list<Eigen::MatrixXd *> unusedMatrixList;
+	std::list<Eigen::MatrixXd *> usedMatrixList;
+	
+	boost::mutex recyclingMutex;
+};
