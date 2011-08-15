6b07e0b2d6d3f84dfaa03a9f0912bb8689971b29
peter <peter.dedecker@hotmail.com>
2011-08-15 11:57:27 -0400

    Add in some arbitrary margin for the detection of saturated pixels

--- PALM_analysis_SOFI.cpp
+++ PALM_analysis_SOFI.cpp
@@ -470,29 +470,32 @@ double SOFICorrector_Order2::functionToMinimize(double psfStdDev, void *params) 
 
 SOFIFrameVerifier_NoSaturation::SOFIFrameVerifier_NoSaturation(int storageType_rhs) {
 	switch (storageType_rhs) {
+            // some systems (e.g. the Nikon TIRF) never report absolutely saturated pixel
+            // values, but instead report a different value close to it
+            // so add in some arbitrary margin for the saturation detection
 		case STORAGE_TYPE_INT8:
-			this->saturationValue = 127.0;
+			this->saturationValue = 127.0 - 1.0;
 			break;
 		case STORAGE_TYPE_UINT8:
-			this->saturationValue = 255.0;
+			this->saturationValue = 255.0 - 2.0;
 			break;
 		case STORAGE_TYPE_INT16:
-			this->saturationValue = 32767.0;
+			this->saturationValue = 32767.0 - 200.0;
 			break;
 		case STORAGE_TYPE_UINT16:
-			this->saturationValue = 65535.0;
+			this->saturationValue = 65535.0 - 200.0;
 			break;
 		case STORAGE_TYPE_INT32:
-			this->saturationValue = 2147483647.0;
+			this->saturationValue = 2147483647.0 - 500.0;
 			break;
 		case STORAGE_TYPE_UINT32:
-			this->saturationValue = 4294967295.0;
+			this->saturationValue = 4294967295.0 - 500.0;
 			break;
 		case STORAGE_TYPE_INT64:
-			this->saturationValue = 9223372036854775807.0;
+			this->saturationValue = 9223372036854775807.0 - 1000.0;
 			break;
 		case STORAGE_TYPE_UINT64:
-			this->saturationValue = 18446744073709551615.0;
+			this->saturationValue = 18446744073709551615.0 - 1000.0;
 			break;
 		case STORAGE_TYPE_INT4:
 		case STORAGE_TYPE_UINT4:
@@ -515,7 +518,7 @@ int SOFIFrameVerifier_NoSaturation::isValidFrame(ImagePtr frame) {
 	
 	double *dataPtr = frame->data();
 	for (size_t i = 0; i < nPixels; i+=1) {
-		if (dataPtr[i] == localSaturationValue) {
+		if (dataPtr[i] >= localSaturationValue) {
 			return 0;
 		}
 	}
