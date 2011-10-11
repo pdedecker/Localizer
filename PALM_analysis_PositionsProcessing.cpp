3a82324a492eb2c3c95c94bfa933d7287ce80a51
peter <peter.dedecker@hotmail.com>
2011-10-10 22:12:19 -0400

    Add GPL license to all source files

--- PALM_analysis_PositionsProcessing.cpp
+++ PALM_analysis_PositionsProcessing.cpp
@@ -1,10 +1,30 @@
 /*
- *  PALM_analysis_PositionsProcessing.cpp
- *  PALM analysis XOP
- *
- *  Created by Peter Dedecker on 06/01/2010.
- *  Copyright 2010 __MyCompanyName__. All rights reserved.
- *
+ Copyright 2008-2011 Peter Dedecker.
+ 
+ This file is part of Localizer.
+ 
+ Localizer is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Localizer is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Localizer.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ 
+ Additional permission under GNU GPL version 3 section 7
+ 
+ If you modify this Program, or any covered work, by 
+ linking or combining it with libraries required for interaction 
+ with analysis programs such as Igor Pro or Matlab, or to acquire
+ data from or control hardware related to an experimental measurement,
+ the licensors of this Program grant you additional permission 
+ to convey the resulting work.
  */
 
 #include "PALM_analysis_PositionsProcessing.h"
